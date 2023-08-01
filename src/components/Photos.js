0fc654e831657309f850cef736de10dc6c1cbb00
mbotela <leemaliy53@gmail.com>
2023-08-02 00:48:51 +0300

    prev commit message

---
+++ src/components/Photos.js
@@ -0,0 +1,18 @@
+// components/Photo.js
+import React from 'react';
+
+const Photo = ({ id, title, source, description, onDelete, onPurchase }) => {
+  return (
+    <div className="photo-card">
+      <img src={source} alt={title} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <div className="button-container">
+        <button onClick={() => onDelete(id)}>Delete</button>
+        <button onClick={() => onPurchase(id)}>Purchase</button>
+      </div>
+    </div>
+  );
+};
+
+export default Photo;
