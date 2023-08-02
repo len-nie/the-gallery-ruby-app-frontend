ea7cd7e26db8cfeae00d27aa895ec0ab110d3ab9
mbotela <leemaliy53@gmail.com>
2023-08-02 12:01:50 +0300

    typescript for jpeg data

---
+++ src/components/data.tsx
@@ -0,0 +1,19 @@
+type Data = {
+    id: number;
+    title: string;
+    source: string;
+  };
+  
+  const photos: Data[] = [
+    { id: 1, title: 'Photo 1', source: '/jpeg/photo1.jpg',},
+    { id: 2, title: 'Photo 2', source: '/jpeg/photo2.jpg',},
+    { id: 3, title: 'Photo 3', source: '/jpeg/photo3.jpg',},
+    { id: 4, title: 'Photo 4', source: '/jpeg/photo4.jpg',},
+    { id: 5, title: 'Photo 5', source: '/jpeg/photo5.jpg',},
+    { id: 6, title: 'Photo 6', source: '/jpeg/photo6.jpg',},
+    { id: 7, title: 'Photo 7', source: '/jpeg/photo7.jpg',},
+    { id: 8, title: 'Photo 8', source: '/jpeg/photo8.jpg',},
+    { id: 9, title: 'Photo 9', source: '/jpeg/photo9.jpg',}
+  ];
+  
+  export default photos;
