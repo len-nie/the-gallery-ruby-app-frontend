9799f8980d539bd31b183380b2bf4738b392feb6
mbotela <leemaliy53@gmail.com>
2023-08-02 08:23:07 +0300

    repaired redundancy

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Gallery = () => {
+  const imageNumbers = Array.from({ length: 9 }, (_, index) => index + 1);
+
   return (
     <div>
-      {/* Reference the image using the public folder path */}
-      <img src="src/jpeg/photo1.jpg" alt="My Image" />
+      {imageNumbers.map((number) => (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img key={number} src={`src/jpeg/photo${number}.jpg`} alt={`This is image ${number}`} />
+      ))}
     </div>
   );
 };
